c5754ec50e4e20b01fec617ba3bb6a07f88444bb
Valentyna Kucheriava <157480181+ValentynaKucheriava@users.noreply.github.com>
2024-02-27 12:37:19 +0100

    Update 0. Basic Functions.sql

--- 0. Basic Functions.sql
+++ 0. Basic Functions.sql
@@ -1,6 +1,7 @@
--- ----------------------------------------
--- BASIC FUCNTIONS
--- ----------------------------------------
-
-SELECT COUNT(*)
-FROM people;
+-- query retrieves the maximum price among all pizzas and aliases it as "MaxPrice.
+SELECT MAX (price) AS MaxPrice
+FROM pizzas;
+-- query retrieves the maximum price for each size of pizza available in the table
+SELECT size, MAX (price)
+FROM pizzas
+GROUP BY size;
