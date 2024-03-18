bd85d56540baa9f982d544189b0185800c5d0d39
Valentyna Kucheriava <157480181+ValentynaKucheriava@users.noreply.github.com>
2024-03-18 07:33:10 +0100

    Update 0. Basic Functions.sql

--- 0. Basic Functions.sql
+++ 0. Basic Functions.sql
@@ -1,7 +1,5 @@
--- ----------------------------------------
--- ID 10363 Weekly Orders Report
--- For each week, find the total number of orders. Include only those orders that are from the first quarter of 2023. The output should contain 'week' and 'quantity'.
--- ----------------------------------------
+/* ID 10363 Weekly Orders Report.
+For each week, find the total number of orders. Include only those orders that are from the first quarter of 2023. The output should contain 'week' and 'quantity'. */
 
 SELECT TOP (10) week, SUM (quantity) AS quantity
 FROM orders_analysis
