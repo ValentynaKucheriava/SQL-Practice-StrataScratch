171e0758a8ded6402688889b6bffd410e2669648
Valentyna Kucheriava <157480181+ValentynaKucheriava@users.noreply.github.com>
2024-03-18 08:16:15 +0100

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,1 +1,7 @@
+/* ID 10363 Weekly Orders Report.
+For each week, find the total number of orders. Include only those orders that are from the first quarter of 2023. The output should contain 'week' and 'quantity'. */
 
+SELECT TOP (10) week, SUM (quantity) AS quantity
+FROM orders_analysis
+WHERE week BETWEEN '2023-01-02' and '2023-06-01'
+GROUP BY week 
