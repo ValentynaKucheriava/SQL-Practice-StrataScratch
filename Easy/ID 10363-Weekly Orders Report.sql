08dbc71f35d4cd734dd50dc17ff6fb20be8a2d36
Valentyna Kucheriava <157480181+ValentynaKucheriava@users.noreply.github.com>
2024-03-19 10:28:26 +0100

    Update ID 10363-Weekly Orders Report.sql

--- Easy/ID 10363-Weekly Orders Report.sql
+++ Easy/ID 10363-Weekly Orders Report.sql
@@ -1,4 +1,5 @@
 -- https://platform.stratascratch.com/coding/10363-weekly-orders-report?code_type=5
+
 SELECT TOP (10) week, SUM (quantity) AS quantity
 FROM orders_analysis
 WHERE week BETWEEN '2023-01-02' and '2023-06-01'
