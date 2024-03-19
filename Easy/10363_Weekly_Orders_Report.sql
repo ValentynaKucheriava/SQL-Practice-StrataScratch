c4777de6b8727220616214651fb02eb4f7d1cbbb
Valentyna Kucheriava <157480181+ValentynaKucheriava@users.noreply.github.com>
2024-03-19 11:29:14 +0100

    Create 10363_Weekly_Orders_Report.sql

---
+++ Easy/10363_Weekly_Orders_Report.sql
@@ -0,0 +1,6 @@
+-- https://platform.stratascratch.com/coding/10363-weekly-orders-report?code_type=5
+
+SELECT TOP (10) week, SUM (quantity) AS quantity
+FROM orders_analysis
+WHERE week BETWEEN '2023-01-02' and '2023-06-01'
+GROUP BY week 
