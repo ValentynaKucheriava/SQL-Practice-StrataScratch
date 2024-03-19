0e2cbd691aff9faebf3bd664dc1441673166b31b
Valentyna Kucheriava <157480181+ValentynaKucheriava@users.noreply.github.com>
2024-03-19 10:26:13 +0100

    Delete solution.sql

--- solution.sql
+++
@@ -1,7 +0,0 @@
--- Sourse: https://platform.stratascratch.com/coding/10363-weekly-orders-report?code_type=5
-# ID 10363 Weekly Orders Report
-  
-SELECT TOP (10) week, SUM (quantity) AS quantity
-FROM orders_analysis
-WHERE week BETWEEN '2023-01-02' and '2023-06-01'
-GROUP BY week 
