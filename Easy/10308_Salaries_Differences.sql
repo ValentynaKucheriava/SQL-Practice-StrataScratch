0a464bbff13a8a111095734686bba512f88abe69
Valentyna Kucheriava <157480181+ValentynaKucheriava@users.noreply.github.com>
2024-03-20 10:19:48 +0100

    Update 10308_Salaries_Differences.sql

--- Easy/10308_Salaries_Differences.sql
+++ Easy/10308_Salaries_Differences.sql
@@ -1,15 +1,15 @@
 -- https://platform.stratascratch.com/coding/10308-salaries-differences?code_type=5
 
 SELECT
-	ABS ((
-			SELECT MAX(salary) 
-			FROM db_employee emp
-			INNER JOIN db_dept dept ON emp.department_id = dept.id
-			WHERE department = 'marketing
-	) -
-  (
-			SELECT MAX(salary) 
-			FROM db_employee emp
-			INNER JOIN db_dept dept ON emp.department_id = dept.id
-			WHERE department = 'engineering '
-		)) AS salary_difference; 
+    ABS((
+        SELECT MAX(salary) 
+        FROM db_employee AS emp
+        INNER JOIN db_dept AS dept ON emp.department_id = dept.id
+        WHERE department = 'marketing'
+    ) -
+    (
+        SELECT MAX(salary) 
+        FROM db_employee AS emp
+        INNER JOIN db_dept AS dept ON emp.department_id = dept.id
+        WHERE department = 'engineering'
+    )) AS salary_difference;
